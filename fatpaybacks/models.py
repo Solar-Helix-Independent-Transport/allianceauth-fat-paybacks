728571b436d5875d850cc056928c0569ba7a34f2
Aaron Kable <aaronkable@gmail.com>
2024-01-21 14:54:46 +0800

    Add distinct

--- fatpaybacks/models.py
+++ fatpaybacks/models.py
@@ -44,7 +44,7 @@ class FatPaybackSetup(models.Model):
         ).values(
             # Group by main character
             "character__character_ownership__user__profile__main_character__character_id",
-        ).distinct().annotate(
+        ).annotate(
             # Count fats per main
             fats=Count("id"),
             # corp id for simplicity
@@ -53,16 +53,16 @@ class FatPaybackSetup(models.Model):
         ).filter(
             # Only those that match the threshold
             fats__gte=self.active_threshold
-        )
+        ).distinct()
 
     def get_active_counts_per_corp(self):
         char_data = self.get_character_fleet_data()
         return char_data.values(
             "corp"
-        ).distinct().annotate(
+        ).annotate(
             actives=Count(
                 "character__character_ownership__user__profile__main_character__character_id")
-        )
+        ).distinct()
 
     def get_income_total(self):
         total_share = 0
